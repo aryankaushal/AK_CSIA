9d4aff6ac586c9f9dd9b21e908adc03687d92be0
aryankaushal <31799043+aryankaushal@users.noreply.github.com>
2024-01-28 09:19:38 +0530

    yet to: fix sign in

--- login.py
+++ login.py
@@ -17,10 +17,11 @@ from PyQt5.QtWidgets import (
     QTextBrowser,
     QMessageBox,
 )
-from PyQt5.QtGui import QPixmap, QColor
+from PyQt5.QtGui import QPixmap, QColor, QFont
 from PyQt5.QtCore import pyqtSignal, Qt, QTimer
 from email.mime.text import MIMEText
 from random import randint, sample
+
 # from UI import LearnUI, TestUI, HomeUI
 # import time
 # import datetime
@@ -34,19 +35,42 @@ class SignInPage(QDialog):
     def __init__(self, database_conn):
         super().__init__()
 
+        self.setWindowTitle("Climaware Sign In")
+        self.setGeometry(512, 100, 500, 900)
+        self.setStyleSheet("background-color: #B5B5C8;")
+        self.reg_title = QLabel("\nWelcome back to\nClimaware!")
+        self.reg_title.setFont(QFont("Arial", 40, QFont.Bold))
+        self.reg_title.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
+        self.reg_title.setStyleSheet("color: black;")
+
+        self.instruction_label = QLabel("Enter your username and password to sign in!")
+        self.instruction_label.setFont(QFont("Arial", 25))
+        self.instruction_label.setStyleSheet("color: black;")
+        self.instruction_label.setAlignment(Qt.AlignHCenter)
+
         self.database_conn = database_conn
 
         self.username_label = QLabel("Username:")
         self.username_input = QLineEdit()
+        self.username_label.setFont(QFont("Arial", 25, QFont.Bold))
+        self.username_label.setStyleSheet("color: black;")
 
         self.password_label = QLabel("Password:")
         self.password_input = QLineEdit()
         self.password_input.setEchoMode(QLineEdit.Password)
+        self.password_label.setFont(QFont("Arial", 25, QFont.Bold))
+        self.password_label.setStyleSheet("color: black;")
 
         self.sign_in_button = QPushButton("Sign In")
         self.sign_in_button.clicked.connect(self.sign_in_user)
+        self.sign_in_button.setFont(QFont("Arial", 25, QFont.Bold))
+        self.sign_in_button.setStyleSheet(
+            "background-color: #1F94D3; color: black; border-radius: 20px; font-size: 30px; min-width: 30; min-height: 50px;"
+        )
 
         layout = QVBoxLayout()
+        layout.addWidget(self.reg_title)
+        layout.addWidget(self.instruction_label)
         layout.addWidget(self.username_label)
         layout.addWidget(self.username_input)
         layout.addWidget(self.password_label)
@@ -54,7 +78,7 @@ class SignInPage(QDialog):
         layout.addWidget(self.sign_in_button)
 
         self.setLayout(layout)
-
+    
     def sign_in_user(self):
         username = self.username_input.text()
         password = self.password_input.text()
@@ -73,14 +97,30 @@ class SignInPage(QDialog):
             self.accept()  # Close the SignInPage
         else:
             QMessageBox.warning(
-                self, "Invalid Credentials", "Invalid username or password."
+                self, "Invalid Credentials", "Invalid username and/or password."
             )
+
 
 class RegistrationPage(QDialog):
     registration_successful = pyqtSignal()
 
     def __init__(self, database_conn):
         super().__init__()
+
+        self.setWindowTitle("Climaware Registration")
+        self.setGeometry(512, 100, 500, 900)
+        self.setStyleSheet("background-color: #B5B5C8;")
+        self.reg_title = QLabel("\nWelcome to\nClimaware!")
+        self.reg_title.setFont(QFont("Arial", 40, QFont.Bold))
+        self.reg_title.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
+        self.reg_title.setStyleSheet("color: black;")
+
+        self.instruction_label = QLabel(
+            "Enter your desired username and password to join us!"
+        )
+        self.instruction_label.setFont(QFont("Arial", 25))
+        self.instruction_label.setStyleSheet("color: black;")
+        self.instruction_label.setAlignment(Qt.AlignHCenter)
 
         # print("INSIDE REG CLASS")
         self.database_conn = database_conn
@@ -89,29 +129,53 @@ class RegistrationPage(QDialog):
 
         self.username_label = QLabel("Username:")
         self.username_input = QLineEdit()
+        self.username_label.setFont(QFont("Arial", 25, QFont.Bold))
+        self.username_label.setStyleSheet("color: black;")
+
+        self.instruction2_label = QLabel(
+            "Password must:\n1. Be at least 8 characters long\n2. Contain at least one uppercase letter, one lowercase letter,\none digit, and one special character (#, _, /)."
+        )
+        self.instruction2_label.setFont(QFont("Arial", 14))
+        self.instruction2_label.setStyleSheet("color: #444444;")
 
         self.password_label = QLabel("Password:")
         self.password_input = QLineEdit()
         self.password_input.setEchoMode(QLineEdit.Password)
+        self.password_label.setFont(QFont("Arial", 30, QFont.Bold))
+        self.password_label.setStyleSheet("color: black;")
 
         self.confirm_password_label = QLabel("Confirm Password:")
         self.confirm_password_input = QLineEdit()
         self.confirm_password_input.setEchoMode(QLineEdit.Password)
+        self.confirm_password_label.setFont(QFont("Arial", 25, QFont.Bold))
+        self.confirm_password_label.setStyleSheet("color: black;")
 
         self.password_strength_label = QLabel()
         self.password_strength_label.setAlignment(Qt.AlignCenter)
+        self.password_label.setFont(QFont("Arial", 14, QFont.Bold))
 
         self.register_button = QPushButton("Join us!")
         self.register_button.clicked.connect(self.register_user)
+        self.register_button.setFont(QFont("Arial", 25, QFont.Bold))
+        self.register_button.setStyleSheet(
+            "background-color: #1FD38C; color: black; border-radius: 20px; font-size: 30px; min-width: 30; min-height: 50px;"
+        )
 
         self.sign_in_button = QPushButton("Sign In, instead")
         self.sign_in_button.clicked.connect(self.show_sign_in_page)
+        self.sign_in_button.setFont(QFont("Arial", 25, QFont.Bold))
+        self.sign_in_button.setStyleSheet(
+            "background-color: #1F94D3; color: black; border-radius: 20px; font-size: 30px; min-width: 30; min-height: 50px;"
+        )
 
         # switch_to_sign_in_page = pyqtSignal()
 
         layout = QVBoxLayout()
+        layout.addWidget(self.reg_title)
+        layout.addWidget(self.instruction_label)
         layout.addWidget(self.username_label)
         layout.addWidget(self.username_input)
+        layout.addWidget(self.instruction2_label)
         layout.addWidget(self.password_label)
         layout.addWidget(self.password_input)
         layout.addWidget(self.confirm_password_label)
@@ -125,8 +189,8 @@ class RegistrationPage(QDialog):
         self.password_input.textChanged.connect(self.update_password_strength)
 
     def show_sign_in_page(self):
-            page = SignInPage(self.database_conn)
-            page.exec_()
+        page = SignInPage(self.database_conn)
+        page.exec_()
 
     def update_password_strength(self):
         password = self.password_input.text()
@@ -140,8 +204,12 @@ class RegistrationPage(QDialog):
             "strong": "green",
         }
 
-        self.password_strength_label.setText(f"Password Strength: {strength.capitalize()}")
-        self.password_strength_label.setStyleSheet(f"color: {colors[strength]}; font-weight: bold;")
+        self.password_strength_label.setText(
+            f"Password Strength: {strength.capitalize()}"
+        )
+        self.password_strength_label.setStyleSheet(
+            f"color: {colors[strength]}; font-weight: bold;"
+        )
 
     def calculate_password_strength(self, password):
         criteria_met = 0
@@ -203,6 +271,11 @@ class RegistrationPage(QDialog):
         )
         self.database_conn.commit()
         print("Registration successful!")
+
+        # QMessageBox.information(
+        #     self, "Registration Successful", 
+        #     "Congratulations! Registration successful!\nClose this, run the app, and use the Sign In button"
+        # )
 
         self.registration_successful.emit()
         # Close the RegistrationPage
--- main.py
+++ main.py
@@ -106,7 +106,12 @@ class MainWindow(QWidget):
         self.registration_page = login.RegistrationPage(self.database_conn)
         self.stacked_widget = QStackedWidget()
         self.stacked_widget.addWidget(self.registration_page)
-        print("I AM EHREREREE in main after calling reg")
+        
+        # added:
+        self.sign_in_page = login.SignInPage(self.database_conn)
+        self.stacked_widget.addWidget(self.sign_in_page)
+
+        # print("I AM EHREREREE in main after calling reg")
 
         self.home_page = HomePage()
 
@@ -114,9 +119,12 @@ class MainWindow(QWidget):
         self.setStyleSheet("background-color: lightblue;")
 
         # # self.sign_in_page = SignInPage(self.database_conn)
+        self.registration_page.registration_successful.connect(
+            self.show_registration_success_message
+        )
 
         # self.registration_page.registration_successful.connect(self.show_home_page)
-        # self.sign_in_page.sign_in_successful.connect(self.show_home_page)
+        self.sign_in_page.sign_in_successful.connect(self.sign_in_page.show_home_page)
 
         layout = QVBoxLayout()
         layout.addWidget(self.stacked_widget)
@@ -124,8 +132,16 @@ class MainWindow(QWidget):
         layout.addStretch(1)
         self.setLayout(layout)
 
+    def show_registration_success_message(self):
+        QMessageBox.information(
+            self,
+            "Registration Successful",
+            "Congratulations!\nClose this, run the app again, and select the Sign In button.",
+        )
         # self.home_page.learn_button.clicked.connect(self.show_registration_page)
         # self.home_page.test_button.clicked.connect(self.show_sign_in_page)
+
+    
 
     def show_learn_page(self):
         learn_page = learn.LearnPage()
